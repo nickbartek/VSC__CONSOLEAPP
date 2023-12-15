c18c299add74a03b29e48f6c281184c1c466c921
tutajbartek@gmail.com <tutajbartek@gmail.com>
2023-12-15 02:18:19 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string email;
+        bool isValid = false;
+
+        do
+        {
+            Console.WriteLine("Podaj adres e-mail do walidacji:");
+            email = Console.ReadLine();
+
+            if (IsValidEmail(email))
+            {
+                isValid = true;
+                Console.WriteLine("Adres e-mail jest poprawny.");
+            }
+            else
+            {
+                Console.WriteLine("Adres e-mail jest niepoprawny. Spróbuj ponownie.");
+            }
+        } while (!isValid);
+    }
+
+    static bool IsValidEmail(string email)
+    {
+        string pattern = @"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+        return Regex.IsMatch(email, pattern);
+    }
+}
---
+++ using System;.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string email;
+        bool isValid = false;
+
+        do
+        {
+            Console.WriteLine("Podaj adres e-mail do walidacji:");
+            email = Console.ReadLine();
+
+            if (IsValidEmail(email))
+            {
+                isValid = true;
+                Console.WriteLine("Adres e-mail jest poprawny.");
+            }
+            else
+            {
+                Console.WriteLine("Adres e-mail jest niepoprawny. Spróbuj ponownie.");
+            }
+        } while (!isValid);
+    }
+
+    static bool IsValidEmail(string email)
+    {
+        string pattern = @"^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$";
+        return Regex.IsMatch(email, pattern);
+    }
+}
